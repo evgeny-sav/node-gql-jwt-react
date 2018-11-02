ca7a3d8b531cab712ec331a54288e6b41132718d
sauchankay <yauheni.sauchanka@dowjones.com>
2018-11-02 15:53:33 +0300

    testing prettier

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
-import express from "express";
-import mongoose from "mongoose";
-import cors from "cors";
-import helmet from "helmet";
-import bodyParser from "body-parser";
-import { format, createLogger, transports } from "winston";
-import morgan from "morgan";
-import graphqlHTTP from "express-graphql";
-import config from "./configs/config";
-import schema from "./scr/schema";
-const { combine, timestamp, label, printf } = format;
+import express from 'express'
+import mongoose from 'mongoose'
+import cors from 'cors'
+import helmet from 'helmet'
+import bodyParser from 'body-parser'
+import { format, createLogger, transports } from 'winston'
+import morgan from 'morgan'
+import graphqlHTTP from 'express-graphql'
+import config from './configs/config'
+import schema from './scr/schema'
+const { combine, timestamp, label, printf } = format
 
 /*
  * TODO: mongoose schemas ?
@@ -25,53 +25,53 @@ const { combine, timestamp, label, printf } = format;
 
 const logger = createLogger({
   format: combine(
-    label({ label: "API" }),
+    label({ label: 'API' }),
     timestamp(),
     printf(info => {
       return `${info.timestamp} [${info.label}:${info.level.toUpperCase()}] : ${
         info.message
-      }`;
+      }`
     })
   ),
-  transports: [new transports.Console()]
-});
+  transports: [new transports.Console()],
+})
 
-process.on("uncaughtException", err => {
-  logger.error(`Caught exception: ${err}\n`);
-});
+process.on('uncaughtException', err => {
+  logger.error(`Caught exception: ${err}\n`)
+})
 
 mongoose.connect(
   config.MONGO_URI,
   { useNewUrlParser: true }
-);
-const db = mongoose.connection;
-db.on("error", err => logger.error(err));
-db.once("open", () => logger.info("Connected to MongoDB"));
+)
+const db = mongoose.connection
+db.on('error', err => logger.error(err))
+db.once('open', () => logger.info('Connected to MongoDB'))
 
-const app = express();
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use(cors());
-app.use(helmet());
-app.use(morgan(":method :url :status :response-time ms"));
+const app = express()
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(cors())
+app.use(helmet())
+app.use(morgan(':method :url :status :response-time ms'))
 
 // Just example how to use routers
 // import { default as usersRouter } from './scr/routers/users';
 // app.use('/users', usersRouter);
 
 app.use(
-  "/graphql",
+  '/graphql',
   graphqlHTTP({
     schema,
-    graphiql: true
+    graphiql: true,
   })
-);
+)
 
+// eslint-disable-next-line
 app.use((err, req, res, next) => {
-  // eslint-disable-line
-  res.status(err.statusCode || 500).send("Error");
-});
+  res.status(err.statusCode || 500).send('Error')
+})
 
 app.listen(config.API_PORT, err => {
-  logger.info(`Server started at ${config.API_PORT}`);
-});
+  logger.info(`Server started at ${config.API_PORT}`)
+})
